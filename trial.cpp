2db58906aee9950e346a9b05508d1f1ba9f6d249
aaryagawade30 <aaryagawade30@gmail.com>
2025-03-06 14:56:44 +0530

    my commit

---
+++ trial.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream file("maharashtra.csv");
+
+    if(!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+}
